028ed15dc020998e0f84ea44c704ba5b19ec8df0
Red4Ru <mobileandroid95@gmail.com>
2019-07-19 11:21:49 +0300

    bjssdjv

--- auction.py
+++ auction.py
@@ -8,15 +8,16 @@ class Room:
         self.users_array = array_users
         self.auc_step = auc_step
         self.current_cost = min_cost
+        self.send_step()
 
     def get_number_players(self):
         return len(self.users_array)
 
     def next_step(self):
-        self.player_bet(self.users_array[0].step())
+        self.player_bet(self.users_array[0].get_bet())
 
-    def player_bet(self, boolean_var):
-        if boolean_var:
+    def player_bet(self, bet):
+        if bet > self.users_array.get_max_value() + self.auc_step:
             self.users_array.append()
         else:
             self.users_array.pop()
@@ -26,11 +27,10 @@ class Room:
             self.next_step()
 
     def send_step(self):
-        self.users_array.get_min_value(self.auc_step)
+        self.users_array.set_min_value(self.auc_step)
 
     def end_game(self):
         return self.users_array.get_winner()
-
 
 
 class User:
@@ -39,13 +39,10 @@ class User:
         self.name = name
         self.money = money
         self.min_value = 10
-
-    def step(self):
-        a = self.get_bet()
-        # TODO check bet
+        self.bet = 0
 
     def get_bet(self):
-        # TODO: get value by input
+        return input('Enter your bet: ')
 
     def set_min_value(self, value):
         self.min_value = value
@@ -64,9 +61,12 @@ class PlayerChain:
     def get_len(self):
         return len(self.array)
 
-    def get_min_value(self, value):
+    def set_min_value(self, value):
         for i in self.array:
             i.set_min_value(value)
+
+    def get_max_value(self):
+        return self.array[-1].bet
 
     def get_winner(self):
         return self.array[0]
